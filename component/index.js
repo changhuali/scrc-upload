701f5d68a97faecb28e4208063c2938a5e631a3b
scdev <changhua.li@socialcredits.cn>
2018-01-30 11:43:54 +0800

    feat:demo1 completed

--- component/index.js
+++ component/index.js
@@ -2,29 +2,72 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles/upload.less';
 
-export default class Test extends React.Component {
+export default class Upload extends React.Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    accept: PropTypes.string,
+    multiple: PropTypes.bool,
+    disabled: PropTypes.bool,
+    beforeUpload: PropTypes.func,
+    onChange: PropTypes.func.isRequired,
+  }
+  static defaultProps = {
+    accept: '*',
+    multiple: false,
+    disabled: false,
+    beforeUpload: () => true,
   }
   handleClickBubble = () => {
     this.fileInput.click();
   }
-  handleFileRef = (inputRef) => {
+  handleFileChange = (evt) => {
+    const { files } = evt.target;
+    const fileValid = this.checkFile(files);
+    if (fileValid) {
+      this.addFile(files);
+    }
+  }
+  checkFile = (...args) => {
+    const { beforeUpload } = this.props;
+    if (typeof beforeUpload === 'function') {
+      return beforeUpload(...args);
+    }
+    return true;
+  }
+  addFile = (...args) => {
+    const { onChange } = this.props;
+    if (typeof onChange === 'function') {
+      onChange(...args);
+    }
+  }
+  removeFile = () => {
+    this.fileInput.value = '';
+  }
+  saveInputRef = (inputRef) => {
     this.fileInput = inputRef;
   }
   render() {
+    const {
+      children,
+      accept,
+      multiple,
+      disabled,
+    } = this.props;
     return [
       <span
         key="file-proxy"
         onClick={this.handleClickBubble}
       >
-        {this.props.children}
+        {children}
       </span>,
       <input
         key="file-input"
-        className="scrc-upload-input"
-        ref={this.handleFileRef}
         type="file"
+        className="scrc-upload-input"
+        ref={this.saveInputRef}
+        accept={accept}
+        multiple={multiple}
+        disabled={disabled}
         onChange={this.handleFileChange}
       />,
     ];
--- demo/Demo1.js
+++ demo/Demo1.js
@@ -1,10 +1,59 @@
 import React from 'react';
-import DemoComp from '../component';
+import Upload from '../component';
+import './demo1.less';
 
-export default function Demo1() {
-  return (
-    <DemoComp>
-      click me1
-    </DemoComp>
-  );
+export default class Demo1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      base64File: [],
+    };
+  }
+  beforeUpload = (files) => {
+    console.log(Array.from(files));
+    return true;
+  }
+  onChange = (files) => {
+    this.resetState();
+    Array.from(files).forEach(file => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const newFileList = this.state.base64File.slice();
+        newFileList.push(reader.result);
+        this.setState({
+          base64File: newFileList
+        });
+      }
+    });
+  }
+  genPreview = () => {
+    return this.state.base64File.map((img, index) => {
+      return <img key={index} src={img} alt={`upload_img_${index}`} />;
+    });
+  }
+  resetState = () => {
+    this.setState({
+      base64File: [],
+    });
+  }
+  render() {
+    return [
+      <Upload
+        key="upload"
+        accept="image/*"
+        beforeUpload={this.beforeUpload}
+        onChange={this.onChange}
+        multiple
+      >
+        <button className="demo1-button">上传图片</button>
+      </Upload>,
+      <div
+        key="preview"
+        className="demo1-preview"
+      >
+        {this.genPreview()}
+      </div>
+    ];
+  }
 }
